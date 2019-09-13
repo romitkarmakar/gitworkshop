69d7fac2816b016bca4a94f330131ef58df3a147
omkarmohanty36 <55195168+omkarmohanty36@users.noreply.github.com>
2019-09-14 00:09:50 +0530

    #10 resolved (#56)

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 int i = 0;
 int a = 5;
+/* Omkar Mohanty 19F80101 9002415626 omkarmohanty36@gmail.com
+hey there computer science student at NIT durgapur*/
 // Avishek Bose 19C80072 9749698076 avishek.bose123@gmail.com
 //Aranya Hazra 19D800 9330582701 aranyahazra1@gmail.com
 // gaurav
